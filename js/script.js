df24aa5ea68f9f8755f4680186c4ea1733f55277
Mixail <example@gmail.com>
2023-11-11 13:04:45 +0300

    Nichego netu. Prosto nastroika VS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// a = 15;
+// console.log(a
+// );
+
+
+// let number = 5;
+
+// const poh = 753;
+
+// number += 5;
+// console.log(number);
+// console.log(poh);
+
+const answers = [];
+answers [0] = prompt('Kak ваше имя?', ''); 
+answers [1] = prompt('Как ваше фамилия?', ''); 
+answers [2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
