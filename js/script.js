7b7b587b33af4bab29fc062b40659ce09b0ed15b
Mixail <example@gmail.com>
2023-11-15 21:57:53 +0300

    inception

--- js/script.js
+++ js/script.js
@@ -1,21 +1,92 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// Место для первой задачи
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    
+    while (result.length !== arr.length) {
+        result[result.length] = arr[result.length];
+    }
+    
+    console.log(result);
+    
+    // Не трогаем
+    return result;
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt("Один из последних просмотренных фильмов?");
-const b = prompt("На сколько оцените его?");
-const c = prompt("Один из последних просмотренных фильмов?");
-const d = prompt("На сколько оцените его?");
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === "string") {
+            data[i] += " - done";
+        } else if (typeof(data[i]) === "number") {
+            data[i] *= 2;
+        }
+    }
+    
+    console.log(data);
+
+    // Не трогаем
+    return data;
+}
+
+
+
+
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i];
+    }
+
+    console.log(result);
+
+    // Не трогаем
+    return result;
+}
+
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+const lines = 4;
+let resultStar = "";
+
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < (lines - i)*2; j++) {
+        resultStar += " ";
+    }
+    for (let j = 0; j < i * 2 + 1; j++) {
+        resultStar += "*";
+    }
+    if (i < lines + 1) {
+
+        resultStar += "\n";
+    }
+}
+console.log(resultStar);
+
+// for (let i = 0; i < lines; i++) {
+//     for (let j = 0; j < i * 2 + 1; j++) {
+//         resultStar += "*";
+//     }
+    
+    
+//     resultStar += "\n";
+
+// }
+
+
