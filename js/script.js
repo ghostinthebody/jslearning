8d36d984fb55df19e0fca4b1552e1685a6f6eb9a
Mixail <example@gmail.com>
2023-11-17 17:01:42 +0300

    Tasks_by_function_1

--- js/script.js
+++ js/script.js
@@ -1,47 +1,34 @@
 "use strict";
 
-/*
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-*/
-
-
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-for (let i = 0; i < 2; i++) {
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
-
-if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-} else {
-    console.log("Error");
-    i--;
-};
+// Задача 1
+function sayHello(name) {
+    return ("Привет, " + name + "!");
 }
+console.log(sayHello("Александр"));
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("OSHIBOCHKA");
+// Задача 2
+function returnNeighboringNumbers(number) {
+    let zxc = [number - 1, number, number + 1];
+    return zxc; 
 }
+console.log(returnNeighboringNumbers(3));
 
-console.log(personalMovieDB);
+
+// Задача 3
+function getMathResult(a, b) {
+    if (typeof(b) !== "number" || b <= 0) {
+        return a;
+    } 
+
+    let stroka = ""
+
+    for (let i = 1; i <= b; i++) {
+        if (i === b) {
+            stroka += `${a * i}`;
+        } else {
+            stroka += `${a * i}---`;
+        }
+    }     
+    return stroka;
+}
+console.log(getMathResult(4, 5));
