cd7da1588bfe5b7c609faaefee78dee333081b4a
Mixail <example@gmail.com>
2023-11-23 18:49:41 +0300

    Tasks_by_function_3

--- js/script.js
+++ js/script.js
@@ -1,82 +1,5 @@
 "use strict";
 
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-        
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-rememberMyFilms();
-    
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-detectPersonalLevel();
-    
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const QWE = prompt(`Ваш любимый жанр под номером ${i}`, "");
-        personalMovieDB.genres[i - 1] = QWE;
-    }
-}
-console.log(writeYourGenres());
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);        
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Задача 1
 function sayHello(name) {
     return ("Привет, " + name + "!");
@@ -112,8 +35,8 @@ console.log(getMathResult(4, 5));
 
 // Задача 4
 function calculateVolumeAndArea(data) {
-    if (data % 1 != 0 || typeof(data) != "number" || data <= 0) { /*!Namber.isInteger(data) - определяет целое ли число*/
-        console.log("При вычислении произошла ошибка");
+    if (data % 1 != 0 || typeof (data) != "number" || data < 0) { /*!Number.isInteger(data) - определяет целое ли число*/
+        return "При вычислении произошла ошибка";
     } else {
         let volume = 0;
         let area = 0;
@@ -123,20 +46,51 @@ function calculateVolumeAndArea(data) {
         return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
     }
 }
-console.log(calculateVolumeAndArea(6));
+console.log(calculateVolumeAndArea(0));
 
 // Задача 5
 function getCoupeNumber(data) {
     if (!Number.isInteger(data) || typeof(data) != "number" || data < 0) {
-        console.log("Ошибка. Проверьте правильность введенного номера места");
+        return "Ошибка. Проверьте правильность введенного номера места";
     } else if ( data === 0 || data > 36) {
-        console.log("Таких мест в вагоне не существует");
+        return "Таких мест в вагоне не существует";
     } 
     return Math.ceil(data / 4);
-    
-    
 }
 console.log(getCoupeNumber(23));
+
+// Задача 6
+function getTimeFromMinutes(min) {
+    if (!Number.isInteger(min) || typeof(min) != "number" || min < 0) {
+        return "Ошибка, проверьте данные";
+    } else {
+        let hour = Math.floor(min / 60);
+        let kopecks = min % 60;
+        if (hour === 1) {
+            return `Это ${hour} час и ${kopecks} минут`;
+        } else if (hour > 1 && hour < 5) {
+            return `Это ${hour} часа и ${kopecks} минут`;
+        } else {
+            return `Это ${hour} часов и ${kopecks} минут`;
+        }
+    }
+}
+console.log(getTimeFromMinutes(523));
+
+// Задача 7
+function findMaxNumber(q, w, e, r) {
+    if (typeof (q) !== "number" ||
+        typeof (w) !== "number" ||
+        typeof (e) !== "number" ||
+        typeof (r) !== "number") {
+        return 0;
+    } else {
+    return Math.max(q, w, e, r);
+    }
+}
+console.log(findMaxNumber(3, 42, "3", "32"));
+
+
 
 
 
